7d688d633d06a8a8c6b8156e06ca8b0a53cdffc4
CrashLearner <yonayen@hotmail.com>
2015-12-17 17:54:31 -0800

    Add User ID to Messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,11 +9,11 @@ class MessagesController < ApplicationController
 	end
 
 	def new
-		@message = Message.new
+		@message = current_user.messages.build
 	end
 
 	def create
-		@message = Message.new(message_params)
+		@message = current_user.messages.build(message_params)
 		if @message.save
 			redirect_to root_path
 		else
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215033624) do
+ActiveRecord::Schema.define(version: 20151218014918) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
