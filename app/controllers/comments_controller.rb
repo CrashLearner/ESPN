7d92e7ea503c6dbc15ea9a36ad4f55c0eccd227b
CrashLearner <yonayen@hotmail.com>
2015-12-19 07:55:07 -0800

    Add Styling and Authentication to Controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user!
 	before_action :find_message, only: [:create, :edit, :update, :destroy]
 	before_action :find_comment, only: [:edit, :update, :destroy]
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
 	before_action :find_message, only: [:show, :edit, :update, :destroy]
 
 	def index
